c3599406141b8444cd952ed22c2cc359fa5c4aef
Xilande <danny.james.whittaker@gmail.com>
2024-01-04 21:57:00 -0500

    First commit?

---
+++ src/main/java/net/xilande/relaymod/RelayModClient.java
@@ -0,0 +1,10 @@
+package net.xilande.relaymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RelayModClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
